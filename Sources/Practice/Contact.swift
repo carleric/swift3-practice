1fe6e2a9a811cbddbb5f7d2d45e5dce92e4a2b4e
Carl Bolstad <cbolstad@egencia.com>
2018-03-07 10:27:24 -0800

    simplify Contact problem for use in administering interviews

--- Sources/Practice/Contact.swift
+++ Sources/Practice/Contact.swift
@@ -1,38 +1,19 @@
-// Creating a data structure = Create a collection of contacts, sort by last name, use it in SWIFT
+// write a data structure in Swift that represents a Contact.  It should contain
+// firstname, lastname, email, phone.  Write a test that creates a collection of
+// Contact, sorts them by last name, and prints the full name (firstname lastname).
 import Foundation
 
-protocol Person {
+protocol Contactable {
 	var firstName: String { get }
-	var middleName: String? { get }
 	var lastName: String { get }
 	var fullName: String { get }
-	var age: Int { get set }
 }
 
-public enum NameDisplayStyle {
-	case firstmiddlelast
-	case lastfirst
-	case initials
-}
-
-public struct Contact: Person {
+public struct Contact: Contactable {
 	var firstName: String
-	var middleName: String?
 	var lastName: String
 	var fullName: String {
-		return self.toString()
-	}
-	var age: Int = 0
-
-	public var description: String {
-		return self.fullName
-	}
-
-	public init(firstName:String, middleName:String?, lastName:String)
-	{
-		self.firstName = firstName
-		self.middleName = middleName
-		self.lastName = lastName
+		return "\(firstName) \(lastName)"
 	}
 }
 
@@ -47,70 +28,18 @@ extension Contact: Comparable {
 }
 
 extension Contact: CustomStringConvertible {
-	public func toString(style: NameDisplayStyle = .firstmiddlelast) -> String {
-
-    var outString: String = "blank_space"
-		switch style {
-			case .firstmiddlelast:
-				if let middleName = self.middleName {
-						outString = "\(self.firstName) \(middleName) \(self.lastName)"
-					} else {
-						outString = "\(self.firstName) \(self.lastName)"
-					}
-			case .lastfirst:
-				if let middleName = self.middleName {
-					outString = "\(self.lastName), \(self.firstName) \(middleName)"
-				} else {
-					outString = "\(self.lastName), \(self.firstName)"
-				}
-			case .initials:
-
-        if  let middle = self.middleName?.uppercased().first,
-            let first = self.firstName.uppercased().first,
-            let last = self.lastName.uppercased().first
-        {
-                  outString =  "\(first)\(middle)\(last)"
-        }
-        else
-            if let first = self.firstName.uppercased().first,
-               let last = self.lastName.uppercased().first
-        {
-                  outString =  "\(first)\(last)"
-            }
-        else
-        {
-                outString =  ""
-        }
-      }
-      return outString
-	}
-}
-
-extension String
-{
-    func trim() -> String
-    {
-        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    public var description: String {
+        return self.fullName
     }
 }
 
-
 //tests
 /*
-let contacts: [Contact] =  [Contact(firstName:"Dan", middleName:nil, lastName:"Johnson"),
-							Contact(firstName:"Carl", middleName:"Eric", lastName:"Bolstad"),
-							Contact(firstName:"Bob", middleName:nil, lastName:"Rogers"),
-							Contact(firstName:"Edna", middleName:nil, lastName:"Zimmerman"),
-							Contact(firstName:"Tim", middleName:nil, lastName:"Weimann")]
-
-for contact in contacts.sorted() {
-	//print("\(contact)")
-	print(contact.toString(style:NameDisplayStyle.lastfirst))
-}
-
-var contact1 = Contact(firstName:"Dan", middleName:nil, lastName:"Johnson")
-var contact2 = contact1
-contact2.age = 42
-assert(contact1.age == 42)
-
+ let contacts = [Contact(firstName:"Bob", lastName:"Pullman"),
+                 Contact(firstName:"Dan", lastName:"Zimmerman"),
+                 Contact(firstName:"Alex", lastName:"Johnson"),
+                 Contact(firstName:"Carl", lastName:"Bolstad")]
+                 }
+ 
+ contacts.sorted().forEach({print($0)})
 */
